cd70351a774accb5b0614113169dc1b324751720
archanabharadwaj <archana.bharadwaj@srijan.net>
2022-07-18 01:39:44 +0530

    get user API added

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: 9000,
     dummy_port: 9001,
-    login_service_port: 9002
+    login_service_port: 9002,
+    get_user_port: 9008
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const mb = require('mountebank')
-const settings = require('./settings')
-const helloWorld = require('./helloWorld');
-const loginService = require('./loginService');
+const settings = require('./config/settings')
+const helloWorld = require('./stubs/helloWorld')
+const loginService = require('./stubs/loginService')
+const getUser = require('./stubs/getUser')
 
 const mbServerInstance = mb.create({
     port: settings.port,
@@ -14,4 +15,6 @@ const mbServerInstance = mb.create({
 mbServerInstance.then(function() {
     helloWorld.addService();
     loginService.addService();
+    getUser.addService();
+    
 });

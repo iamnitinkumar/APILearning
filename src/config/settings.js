7e3da40eba0dd22ecbc870b5a8b42af5907c51b2
Ankit Singh <ankit_singh-cwtc@external.mckinsey.com>
2022-07-21 12:59:34 +0530

    fix issues

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,5 +3,4 @@ module.exports = {
     dummy_port: 9001,
     login_service_port: 9002,
     wishlist_service_port: 9006
-
 }
--- src/stubs/wishlistService.js
+++ src/stubs/wishlistService.js
@@ -1,107 +1,78 @@
-const mbHelper = require('../utilities/mountebankHelper')
-const settings = require('../config/settings')
-function addService() {
-	const successResponse = { message: 'Item successfully added' }
-	const unauthorisedResponse = {
-		message: 'User is not authorised to make the request',
-	}
-	const unauthenticatedResponse = {
-		message: 'Authentication information is missing or invalid',
-	}
-	const serviceUnavailableResponse = { message: 'Service not available' }
-	const serviceNotFoundResponse = { message: 'The service is not found' }
-	const stubs = [
-		{
+const mbHelper = require('../utilities/mountebankHelper');
+const settings = require('../config/settings');
+function addWishlistService() {
+	const successResponse = { message: 'Item successfully added' };
+	const unauthorisedResponse = {message: 'User is not authorised to make the request'};
+	const unauthenticatedResponse = {message: 'Authentication information is missing or invalid'};
+	const serviceUnavailableResponse = { message: 'Service not available' };
+	const serviceNotFoundResponse = { message: 'The service is not found' };
+	const stubs = [{
 		//Item successfully added
-			predicates: [
-				{
-					and: [
-						{ equals: { method: 'POST' } },
-						{ startsWith: { path: '/user/auth/wishlist' } },
-						{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
-						{ equals: { body: { itemname: '1' } } },
-					],
-				},
-			],
-			responses: [
-				{
-					is: {
-						statusCode: 201,
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(successResponse),
+		predicates: [{
+			and: [
+				{ equals: { method: 'POST' } },
+				{ startsWith: { path: '/user/auth/wishlist' } },
+				{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
+				{ equals: { body: { itemname: '1' } } },
+				]
+			}],
+			responses: [{
+				is: {
+					statusCode: 201,
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify(successResponse),
 					},
-				},
-			],
+			}],
 			//User is not authorised to make the request
-			predicates: [
-				{
-					and: [
-						{ equals: { method: 'POST' } },
-						{ startsWith: { path: '/user/auth/wishlist' } },
-						{ equals: { Headers: { WWW_Authenticate: '1111' } } },
-						{ equals: { body: { itemname: '1' } } },
-					],
-				},
-			],
-			responses: [
-				{
-					is: {
-						statusCode: 403,
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(unauthorisedResponse),
-					},
-				},
-			],
+		predicates: [{
+			and: [
+				{ equals: { method: 'POST' } },
+				{ startsWith: { path: '/user/auth/wishlist' } },
+				{ equals: { Headers: { WWW_Authenticate: '1111' } } },
+				{ equals: { body: { itemname: '1' } } },
+				]
+			}],
+			responses: [{
+				is: {
+
+					statusCode: 403,
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify(unauthorisedResponse),
+					}
+			}],
 			//Authentication information is missing or invalid
-			predicates: [
-				{
-					and: [
-						{ equals: { method: 'POST' } },
-						{ startsWith: { path: '/user/auth/wishlist' } },
-						{ equals: { Headers: { WWW_Authenticate: '' } } },
-						{ equals: { body: { itemname: '1' } } },
+			predicates: [{
+				and: [
+					{ equals: { method: 'POST' } },
+					{ startsWith: { path: '/user/auth/wishlist' } },
+					{ equals: { Headers: { WWW_Authenticate: '' } } },
+					{ equals: { body: { itemname: '1' } } },
 					],
-				},
-			],
-			responses: [
-				{
-					is: {
-						statusCode: 401,
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(unauthenticatedResponse),
+				}],
+			responses: [{
+				is: {
+					statusCode: 401,
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify(unauthenticatedResponse),
 					},
-				},
-			],
+				}],
 			// Method not allowed
-			predicates: [
-				{
-					and: [
-						{ equals: { method: 'GET' } },
-						{ startsWith: { path: '/user/auth/wishlist' } },
-						{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
-						{ equals: { body: { itemname: '1' } } },
+			predicates: [{
+				and: [
+					{ not: { method: 'POST' } },
+					{ startsWith: { path: '/user/auth/wishlist' } },
+					{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
+					{ equals: { body: { itemname: '1' } } },
 					],
-				},
-			],
-			responses: [
-				{
-					is: {
-						statusCode: 405,
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify(successResponse),
+				}],
+			responses: [{
+				is: {
+					statusCode: 405,
+					headers: {'Content-Type': 'application/json'},
+					body: JSON.stringify(serviceNotFoundResponse),
 					},
-				},
-			],
-		},
-	]
+				}],
+		}]
 	const imposter = {
 		name: 'Wishlist Service',
 		port: settings.wishlist_service_port,
@@ -117,4 +88,4 @@ function addService() {
 	}
 	return mbHelper.postImposter(imposter)
 }
-module.exports = { addService }
+module.exports = { addWishlistService }

1eaf2abef0c78931decffd0e7cbf486fa2ca91f3
Nitin Kumar <nitintrue@gmail.com>
2022-07-28 17:04:21 +1000

    Updated code to fix the stubs not listing correcly

--- src/stubs/wishlistService.js
+++ src/stubs/wishlistService.js
@@ -6,73 +6,73 @@ function addWishlistService() {
 	const unauthenticatedResponse = {message: 'Authentication information is missing or invalid'};
 	const serviceUnavailableResponse = { message: 'Service not available' };
 	const serviceNotFoundResponse = { message: 'The service is not found' };
-	const stubs = [{
-		//Item successfully added
-		predicates: [{
-			and: [
-				{ equals: { method: 'POST' } },
-				{ startsWith: { path: '/user/auth/wishlist' } },
-				{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
-				{ equals: { body: { itemname: '1' } } },
-				]
-			}],
-			responses: [{
-				is: {
-					statusCode: 201,
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify(successResponse),
-					},
-			}],
-			//User is not authorised to make the request
-		predicates: [{
-			and: [
-				{ equals: { method: 'POST' } },
-				{ startsWith: { path: '/user/auth/wishlist' } },
-				{ equals: { Headers: { WWW_Authenticate: '1111' } } },
-				{ equals: { body: { itemname: '1' } } },
-				]
-			}],
-			responses: [{
-				is: {
-
-					statusCode: 403,
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify(unauthorisedResponse),
-					}
-			}],
-			//Authentication information is missing or invalid
+	const stubs = [
+		{
 			predicates: [{
 				and: [
 					{ equals: { method: 'POST' } },
 					{ startsWith: { path: '/user/auth/wishlist' } },
-					{ equals: { Headers: { WWW_Authenticate: '' } } },
+					{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
 					{ equals: { body: { itemname: '1' } } },
-					],
+					]
 				}],
-			responses: [{
-				is: {
-					statusCode: 401,
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify(unauthenticatedResponse),
-					},
-				}],
-			// Method not allowed
-			predicates: [{
-				not: //{
-					{equals :{ method: 'POST' }},
-					//{ startsWith: { path: '/user/auth/wishlist' } },
-					//{ equals: { Headers: { WWW_Authenticate: '1234567890' } } },
-					//{ equals: { body: { itemname: '1' } } },
-				//},
-				}],
-			responses: [{
-				is: {
-					statusCode: 405,
-					headers: {'Content-Type': 'application/json'},
-					body: JSON.stringify(serviceNotFoundResponse),
-					},
-				}],
-		}]
+				responses: [{
+					is: {
+						statusCode: 201,
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify(successResponse),
+						},
+				}]
+			},
+			{
+				predicates: [{
+					and: [
+						{ equals: { method: 'POST' } },
+						{ startsWith: { path: '/user/auth/wishlist' } },
+						{ equals: { Headers: { WWW_Authenticate: '1111' } } },
+						{ equals: { body: { itemname: '1' } } },
+						]
+					}],
+					responses: [{
+						is: {
+		
+							statusCode: 403,
+							headers: {'Content-Type': 'application/json'},
+							body: JSON.stringify(unauthorisedResponse),
+							}
+					}]
+			},
+			{
+				predicates: [{
+					and: [
+						{ equals: { method: 'POST' } },
+						{ startsWith: { path: '/user/auth/wishlist' } },
+						{ equals: { Headers: { WWW_Authenticate: '' } } },
+						{ equals: { body: { itemname: '1' } } },
+						],
+					}],
+				responses: [{
+					is: {
+						statusCode: 401,
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify(unauthenticatedResponse),
+						},
+					}],
+			},
+			{
+				predicates: [{
+					not: 
+						{equals :{ method: 'POST' }},
+					}],
+				responses: [{
+					is: {
+						statusCode: 405,
+						headers: {'Content-Type': 'application/json'},
+						body: JSON.stringify(serviceNotFoundResponse),
+						},
+					}],
+			}
+		]
 	const imposter = {
 		name: 'Wishlist Service',
 		port: settings.wishlist_service_port,
